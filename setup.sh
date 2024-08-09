bbbd54f555738a2d3685a4bc72e91f8ed1fe2bc3
chLi <565353780@qq.com>
2024-08-09 12:41:26 +0800

    finish run with compiled pymesh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,20 @@
-pip install -U open3d opencv-python descartes \
-  matplotlib numpy shapely panda3d
+sudo apt install gcc-multilib g++-multilib
 
-pip install -U pymesh
+cd ../structured-3d/structured_td/Lib/PyMesh/third_party
+./build.py all
+
+cd ..
+mkdir build
+cd build
+cmake ..
+make -j
+# make tests
+
+cd ..
+./setup.py build
+./setup.py install
+
+pip install -U open3d opencv-python descartes \
+  matplotlib shapely panda3d
+
+pip install numpy==1.24.4
